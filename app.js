254f7578a6c27e9b18594ca41a58df2d4c498e8f
Aaron Daniel <aaronmathewdaniel@outlook.com>
2022-02-12 10:30:51 +0530

    app.js file

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ alert(age)
 let hobbies = ["Sports", "Cooking", "Rearl"]
 alert(hobbies[2])
 alert(hobbies[1])
-
-
+let size
+size = "go 300kmph"
+alert(size)
+alert(age)
+let job = {title:"developer", place:"new york"}
 
 
 
