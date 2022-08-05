abf853640481b26605a54e9ce03cf5ea2be051f7
Aarondanielmathew <97221747+Aarondanielmathew@users.noreply.github.com>
2022-08-05 16:40:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-//  display Message
- let age = 777
- alert(age)
+// document.body.children[2].children[0].href = "https://google.com";
 
-//  display objects Message
-let hobbies = ["Sports", "Cooking", "Rearl"]
-alert(hobbies[0])
+// console.dir(document);
+// alert();
+// window.alert();
 
-// display Array Message
-let job = {title:"developer", place:"new york"}
-alert(job.title)
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://udemy.com";
 
-// display mathamatical operations
-let jobconsent = age + 789
-alert(jobconsent)
+let AnchorElement = document.querySelector("#external-link"); // a { color: red; }
+anchorElement.href = "https://academind.com";
 
+// ADD AN ELEMENT
+// 1. Create the new Element
+
+let newAnchorElement = document.createElement("a");
+
+// 2. Get access to the parent element that should hold the new element
+
+let firstParagraph = document.querySelector("p");
+
+// 3. Insert the new element into parent element content
+
+firstParagraph.append(newAnchorElement);
+
+// REMOVE ELEMENTS
+// 1. Select the element that should be removed 
+
+let firstH1Element =  document.querySelector("h1");
+
+// 2. Remove it!
+
+firstH1Element.remove();
+
+// MOVE ELEMENT 
+
+firstParagraph.parentElement.append(firstParagraph);
+
+// InnerHTML
+
+firstParagraph.innerHTML = "Hi This is <strong>important!<strong/>";
 
